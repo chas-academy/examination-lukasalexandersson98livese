ace431c0c345937a31be6ea20beeb2687d6f98ed
lukasalexandersson98livese <lukas.alexandersson98@live.se>
2025-06-01 01:44:01 +0200

    first part

--- main.c
+++ main.c
@@ -1,2 +1,44 @@
 //Write your code in this file
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void read_input(char names[5][11],
+int results[5][13]){
+    for (int i = 0; i < 5; i++){
+    scanf("%s", names[i]);
+    for(int j = 0; j < 13; j++)
+    {
+        scanf("%d", &results[i][j]);
+    
+        }
+       }
+      }
+
+    int calculate_average (int results[13]){
+        int sum = 0;
+        for(int i = 0; i< 13; i++){
+            sum += results[i];
+
+        }
+
+        return sum;
+        }
+
+
+int main(){
+char names[5][11];
+int results[5][13];
+int sum[5];
+
+read_input (names, results);
+
+
+
+
+
+    return 0;
+}
+    
+
 
